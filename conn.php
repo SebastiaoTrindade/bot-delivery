9c7e524e7dd24503c793ea80cd02e79239c65c16
Sebastiao Trindade <sebatrindade@hotmail.com>
2025-04-09 14:29:41 -0300

    Criando o banco de dados

---
+++ conn.php
@@ -0,0 +1,16 @@
+<?php
+
+// Conexão com o banco de dados
+$servername = "localhost";
+$username = "root";
+$password = "adminroot";
+$dbname = "botdelivery";
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Verifica a conexão
+if (!$conn) {
+    echo "Erro na conexão: " . mysqli_connect_error();
+}else{
+    echo "Conexão bem-sucedida";
+}
+?>
